444c89a832ab4730874c78380e3339a0c25bd34e
anxing <anxing131@gmail.com>
2023-09-12 23:41:14 +0800

    chore: lint

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,15 @@ import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { logoChrome } from 'ionicons/icons';
 
 import { io, Manager } from "socket.io-client";
-import * as Isomorphic from 'isomorphic-ws'
+import { default as Isomorphic } from 'isomorphic-ws'
 import {WebSocket as SFClient}  from '@wahr/capacitor-websocket-client'
 
+console.log('Isomorphic ------ : ', Isomorphic)
 import { CapacitorWebsocket } from '@miaz/capacitor-websocket';
 import { Preferences } from '@capacitor/preferences';
 import { Capacitor, CapacitorHttp } from '@capacitor/core';
-import protobuf from 'protobufjs';
-import pako from 'pako'
+import { default as protobuf } from 'protobufjs';
+import { default as pako } from 'pako'
 const sURL = 'wss://webcast3-ws-web-lq.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim|wss_push_room_id:7277885418958261050|wss_push_did:7188358506633528844|dim_log_id:20230521093022204E5B327EF20D5CDFC6|fetch_time:1684632622323|seq:1|wss_info:0-1684632622323-0-0|wrds_kvs:WebcastRoomRankMessage-1684632106402346965_WebcastRoomStatsMessage-1684632616357153318&cursor=t-1684632622323_r-1_d-1_u-1_h-1&host=https://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&maxCacheMessageNumber=20&endpoint=live_pc&support_wrds=1&im_path=/webcast/im/fetch/&user_unique_id=7188358506633528844&device_platform=web&cookie_enabled=true&screen_width=1440&screen_height=900&browser_language=zh&browser_platform=MacIntel&browser_name=Mozilla&browser_version=5.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_15_7)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/113.0.0.0%20Safari/537.36&browser_online=true&tz_name=Asia/Shanghai&identity=audience&room_id=7277885418958261050&heartbeatDuration=0&signature=00000000'
 
 
@@ -26,7 +27,7 @@ startIsomorphic()
 // startSocketIoManager()
 // startWebSocket() 
 
-  async function getCookies(url) {
+  async function getCookies(url: any) {
     const response = await CapacitorHttp.request({
       url,
       webFetchExtra: {
@@ -56,47 +57,47 @@ startIsomorphic()
   async function startCapacitorWebsocket() {
     await getCookies('https://live.douyin.com/668402325818')
     return
-    const socket: any = {
-      onmessage: (msg) => {
-        alert('onmessage inner')
-      }
-    }
-    alert('startCapacitorWebsocket')
-    await CapacitorWebsocket.addListener('default:message', (msg) => {
-      alert('message----------')
-      if (socket.onmessage) {
-        socket.onmessage(msg);
-      }
-    });
-    await CapacitorWebsocket.addListener('default:connected', (msg) => {
-      alert('connected----------')
-    });
-    await CapacitorWebsocket.addListener('default:disconnected', (msg) => {
-       alert('disconnected----------')
-    });
-    await CapacitorWebsocket.addListener('default:statechange', (msg) => {
-      alert('statechange----------')
-    });
-     await CapacitorWebsocket.addListener('default:connecterror', (msg) => {
-      alert('connecterror----------')
-    });
-     await CapacitorWebsocket.addListener('default:error', (msg) => {
-      alert('error----------')
-    });
-    const { value } = await Preferences.get({ key: 'cookie' });
-    alert('value -2- : ' + value)
-    await CapacitorWebsocket.build({
-      name: 'default',
-      url: sURL,
-      // headers: { Cookie: value },
-      headers: {
-        'Cookie': value,
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-      }
-    });
+    // const socket: any = {
+    //   onmessage: (msg) => {
+    //     alert('onmessage inner')
+    //   }
+    // }
+    // alert('startCapacitorWebsocket')
+    // await CapacitorWebsocket.addListener('default:message', (msg) => {
+    //   alert('message----------')
+    //   if (socket.onmessage) {
+    //     socket.onmessage(msg);
+    //   }
+    // });
+    // await CapacitorWebsocket.addListener('default:connected', (msg) => {
+    //   alert('connected----------')
+    // });
+    // await CapacitorWebsocket.addListener('default:disconnected', (msg) => {
+    //    alert('disconnected----------')
+    // });
+    // await CapacitorWebsocket.addListener('default:statechange', (msg) => {
+    //   alert('statechange----------')
+    // });
+    //  await CapacitorWebsocket.addListener('default:connecterror', (msg) => {
+    //   alert('connecterror----------')
+    // });
+    //  await CapacitorWebsocket.addListener('default:error', (msg) => {
+    //   alert('error----------')
+    // });
+    // const { value } = await Preferences.get({ key: 'cookie' });
+    // alert('value -2- : ' + value)
+    // await CapacitorWebsocket.build({
+    //   name: 'default',
+    //   url: sURL,
+    //   // headers: { Cookie: value },
+    //   headers: {
+    //     'Cookie': value,
+    //     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    //   }
+    // });
 
-    await CapacitorWebsocket.applyListeners({ name: 'default' });
-    await CapacitorWebsocket.connect({ name: 'default' });
+    // await CapacitorWebsocket.applyListeners({ name: 'default' });
+    // await CapacitorWebsocket.connect({ name: 'default' });
   }
 
   async function startSFSocketIo() {
@@ -128,7 +129,7 @@ startIsomorphic()
 
   async function startIsomorphic() {
     await startCapacitorWebsocket()
-    const ws = new Isomorphic.default(sURL);
+    const ws = new Isomorphic(sURL);
 
     ws.onopen = function open() {
       console.log('connected');
